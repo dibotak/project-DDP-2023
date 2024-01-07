7aa155096669a56abadb11d34d71a9f1e0254033
Muhammad Adib Faqih <dev@adibfaqih.id>
2024-01-08 02:00:37 +0700

    added error handling

--- duit.py
+++ duit.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
 
 root = Tk()
 root.title("Hitung Hitung")
@@ -7,15 +8,21 @@ root.title("Hitung Hitung")
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 
-def add_data(_args):
+def add_data(_args = False):
     global total_uang
     print( 'jumlah uang', jumlah_uang.get())
     print( 'keterangan', keterangan.get())
-    total_uang.set(f"Total: {str(int(total_uang.get().split(' ')[1]) + int(jumlah_uang.get()))}")
-    list_uang.insert('end', f'Rp. {jumlah_uang.get()}, {keterangan.get()}')
+
+    try:
+        total_uang = int(total_uang) + int(jumlah_uang.get())
+        total_uang_text.set(f"Total: Rp. {str(total_uang)}")
+        list_uang.insert('end', f'Rp. {jumlah_uang.get()}, {keterangan.get()}')
     
-    jumlah_uang.set('')
-    keterangan.set('')
+        jumlah_uang.set('')
+        keterangan.set('')
+    except:
+        messagebox.showerror("Ups, ada error", "Harap isi jumlah uang dengan angka")
+    
 
 leftframe = ttk.Frame(mainframe, padding="3 3 12 12")
 leftframe.grid(column=0, row=0, sticky=(N, W, E, S))
@@ -40,8 +47,9 @@ s = ttk.Scrollbar(mainframe, orient=VERTICAL, command=list_uang.yview)
 s.grid(column=2, row=0, sticky=(N,S))
 list_uang['yscrollcommand'] = s.set
 
-total_uang = StringVar(value="Total: 0")
-ttk.Label(root, textvariable=total_uang).grid(column=0, row=1)
+total_uang = 0
+total_uang_text = StringVar(value=f"Total: Rp. {total_uang}")
+ttk.Label(root, textvariable=total_uang_text).grid(column=0, row=1)
 
 mainframe.columnconfigure(0, weight=1)
 
